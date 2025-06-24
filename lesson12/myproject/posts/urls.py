4f4e61fa354e50edbf6834387bcbb90671bb7909
Mohamad Maleki <51901591+maleki22@users.noreply.github.com>
2025-06-24 07:50:23 +0330

    Update urls.py

--- lesson12/myproject/posts/urls.py
+++ lesson12/myproject/posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.posts_list, name="list"),
+    path('', views.post_list, name="list"),
     path('new-post/', views.post_new, name="new-post"),
     path('<slug:slug>', views.post_page, name="page"),
 ]
